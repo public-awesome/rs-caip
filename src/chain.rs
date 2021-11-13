10d9a6019241f78220eefd56beab8736f57a6817
Shane Vitarana <shanev@gmail.com>
2021-11-13 16:50:32 +0000

    CAIP-2: ChainId

---
+++ src/chain.rs
@@ -0,0 +1,28 @@
+// use std::fmt::{self};
+
+// const CAIP: &str = "caip-2";
+
+// #[derive(Debug, PartialEq)]
+// struct ChainId {
+//   namespace: String,
+//   reference: String,
+// }
+
+// impl FromStr for ChainId {
+//   fn from_str(s: &str) -> Result<Self, Self::Err> {
+//     let params = chainId.split(":").collect();
+//     // Ok(ChainId { namespace: params[0], reference: params[1] })
+//     Ok()
+//   }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//   #[test]
+//   fn it_works() {
+//     let id = ChainId::from_str("cosmos:stargaze-3")
+//     println!("{}", id)
+//     println!("hello hello")
+//     assert_eq!(id.unwrap(), ChainId{ namespace: "cosmos", reference: "stargaze-3"})
+//   }
+// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,55 @@
+use std::fmt;
+use std::str::FromStr;
+
+#[derive(Debug, Clone, PartialEq)]
+struct ChainIdError;
+
+#[derive(Debug, PartialEq)]
+struct ChainId {
+  namespace: String,
+  reference: String,
+}
+
+impl fmt::Display for ChainIdError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "invalid chain id")
+  }
+}
+
+impl FromStr for ChainId {
+  type Err = ChainIdError;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    let params: Vec<&str> = s.split(":").collect();
+    if params.len() != 2 {
+      return Err(ChainIdError);
+    }
+    Ok(ChainId {
+      namespace: params[0].to_string(),
+      reference: params[1].to_string(),
+    })
+  }
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+  use crate::{ChainId, ChainIdError, FromStr};
+
+  #[test]
+  fn valid_chain_id() {
+    let id = ChainId::from_str("cosmos:stargaze-3");
+    assert_eq!(
+      id.unwrap(),
+      ChainId {
+        namespace: "cosmos".to_string(),
+        reference: "stargaze-3".to_string()
+      }
+    )
+  }
+
+  #[test]
+  fn invalid_chain_id() {
+    let id = ChainId::from_str("cosmos:stargaze-3:invalid");
+    assert_eq!(id.unwrap_err(), ChainIdError)
+  }
 }
